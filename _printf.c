1f0e1dd651d3e10b794d0ccf89d82352ec9f59f6
carlalap <carlos3_alarcon@hotmail.com>
2023-03-09 10:41:09 -0800

    0Task

--- _printf.c
+++ _printf.c
@@ -23,7 +23,7 @@ int _printf(const char * const format, ...)
 Here:
 	while (format[i] != '\0')
 	{
-		j = 13;
+		j = 6;
 		while (j >= 0)
 		{
 			if (m[j].id[0] == format[i] && m[j].id[1] == format[i + 1])
