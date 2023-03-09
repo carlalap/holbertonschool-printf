ba5a8f2cb5ae51dc94189cbd9aaf1a3d59c0c729
carlalap <carlos3_alarcon@hotmail.com>
2023-03-09 12:29:20 -0800

    task0

--- main.h
+++ main.h
@@ -1,18 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <stdarg.h>
-#include <sys/wait.h>
-#include <sys/stat.h>
-#include <sys/types.h>
 #include <limits.h>
-#include <fcntl.h>
-#include <errno.h>
-#include <signal.h>
+#include <unistd.h>
+
 
 /**
 * struct format - match the conversion specifiers for printf
@@ -27,10 +21,24 @@ char *id;
 int (*f)();
 } convert_match;
 
+int printf_pointer(va_list val);
+int printf_hex_aux(unsigned long int num);
+int printf_HEX_aux(unsigned int num);
+int printf_exclusive_string(va_list val);
+int printf_HEX(va_list val);
+int printf_hex(va_list val);
+int printf_oct(va_list val);
+int printf_unsigned(va_list args);
+int printf_bin(va_list val);
+int printf_srev(va_list args);
+int printf_rot13(va_list args);
+int printf_int(va_list args);
+int printf_dec(va_list args);
 int _strlen(char *s);
 int *_strcpy(char *dest, char *src);
 int _strlenc(const char *s);
 int rev_string(char *s);
+int _strlenc(const char *s);
 int printf_37(void);
 int printf_char(va_list val);
 int printf_string(va_list val);
@@ -38,3 +46,4 @@ int _putchar(char c);
 int _printf(const char *format, ...);
 
 #endif
+
