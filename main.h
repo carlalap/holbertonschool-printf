a0c906832e44ff54e00a8ac719d01dfe7a9b739e
carlalap <carlos3_alarcon@hotmail.com>
2023-03-10 08:16:51 -0800

    update

--- main.h
+++ main.h
@@ -23,13 +23,16 @@ int (*f)();
 
 int printf_int(va_list args);
 int printf_dec(va_list args);
-int _strlen(char *s);
 int *_strcpy(char *dest, char *src);
+int _strlen(char *s);
 int _strlenc(const char *s);
+int printf_37(void);
 int printf_char(va_list val);
 int printf_string(va_list val);
 int _putchar(char c);
 int _printf(const char *format, ...);
+
+
 
 #endif
 
